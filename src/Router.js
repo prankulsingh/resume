72496028d24d7bcf051b1ac2d7babc0527a10693
prankulsingh <prankulsinghps@gmail.com>
2022-06-28 22:15:02 +0530

    new changes

---
+++ src/Router.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
+import App from "./App";
+
+export default function PageRouter() {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <App/>
+                </Route>
+                <Route path="/meet">
+                    <OpenMeeting url = "https://meet.google.com/hih-zjrs-was"/>
+                </Route>
+            </Switch>
+        </Router>
+    );
+
+    function OpenMeeting({url}) {
+        window.location.replace(url);
+        return null;
+    }
+}
